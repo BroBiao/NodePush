a0f800a1c4406b0b35f92a9c51428d21cce0e693
BroBiao <brobiao29@gmail.com>
2025-04-10 16:20:42 +0800

    update mac script, fix bug

--- w.ai/wai_restart_mac.sh
+++ w.ai/wai_restart_mac.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
 APP_NAME="w ai"
+APP_PATH="/Applications/w ai.app/Contents/MacOS/w ai"
 SERV_NAME=$(launchctl list | grep application.ai.wombo.wai | awk '{print $3}')
 LOG_PATH="/Users/debiao/.wombo/cache/client.log"
-MAX_SECONDS=300
+MAX_SECONDS=100
 
 now_time=$(date "+%m-%d %H:%M:%S")
 log_modtime=$(stat -f %m "$LOG_PATH")
@@ -12,8 +13,11 @@ time_diff=$((now_ts - log_modtime))
 if [ $time_diff -gt $MAX_SECONDS ]; then
     echo "$now_time No new logs for $time_diff seconds, trying to restart..."
     if pgrep -xq "$APP_NAME"; then
+	pgid=$(pgrep -f "$APP_PATH")
 	launchctl stop $SERV_NAME
-	sleep 30
+	sleep 3
+	kill -TERM -$pgid
+	sleep 10
     fi
     open -a "$APP_NAME"
 else
